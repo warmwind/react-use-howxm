4095e044f779df266392d4258fb39e4a6e8e1605
fang.yang <yangfang2028@126.com>
2023-03-24 11:24:00 +0800

    📦️ Upgrade publish config.

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import external from 'rollup-plugin-peer-deps-external'
-import url from '@rollup/plugin-url'
+import { terser } from 'rollup-plugin-terser'
 
 import pkg from './package.json'
 
@@ -13,20 +13,14 @@ export default {
       file: pkg.main,
       format: 'cjs',
       exports: 'named',
-      sourcemap: true,
+      sourcemap: false,
     },
     {
       file: pkg.module,
       format: 'es',
       exports: 'named',
-      sourcemap: true,
+      sourcemap: false,
     },
   ],
-  plugins: [
-    external(),
-    url({ exclude: ['**/*.svg'] }),
-    resolve(),
-    typescript(),
-    commonjs({ extensions: ['.js', '.ts'] }),
-  ],
+  plugins: [external(), resolve(), typescript(), commonjs({ extensions: ['.js', '.ts'] }), terser()],
 }
